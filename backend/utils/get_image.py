b80440fa789c7d5956ef7b39bc57fdcce334f064
anirudh chebolu <ani1005sai@gmail.com>
2022-10-23 07:46:17 +0530

    background image in videos

--- backend/utils/get_image.py
+++ backend/utils/get_image.py
@@ -1,7 +1,7 @@
 import requests
 import base64
 import os
-
+from PIL import Image,ImageDraw,ImageFont
 THIS_FILE = os.path.dirname(os.path.abspath(__file__))
 PROJ_FILE = os.path.abspath(os.path.join(THIS_FILE,"../"))
 class GetImage:
@@ -27,6 +27,24 @@ class GetImage:
             for chunk in response:
                 f.write(chunk)
             f.close()
+        self.add_background(path)
+    
+    def add_background(self,path):
+        lpath = [x for x in path.split('/')]
+        name = lpath[-1][:-4]
+        bg_path = PROJ_FILE + "/images/bg.png" 
+        bg_img = Image.open(bg_path).convert('RGB')
+        ht = bg_img.height
+        width = bg_img.width
+        def generate():
+            image = Image.open(path).convert('RGB')
+            image = image.resize(((2*width//3) + 20, 2*ht//3))
+            bg_img.paste(image,(120,120))
+            I1 = ImageDraw.Draw(bg_img)
+            #font = ImageFont.truetype('arial.ttf',45)
+            I1.text(((width//2), 2*ht//3 + 200),name,font = font ,fill = 'rgb(254, 1 , 1)')
+            bg_img.save(path)
+        generate()
         
 test = GetImage("job2","AcYSjRh6wf7hgpIuaUcS3aoQkwTasQqUVVh13068pfqjvk-r-K6xJ1CxXK85Agzp-krT8vOHDc38Fa8ADKIxNgrSjk1ypBanDe2P1DvdieUZq23UnMpX9_34gBCfZueVlUMcRRrIx8xjFSB7NpC1Cl-bst8UteEVC9ADDxMluZ29LfYWQOwV","panda")
 
